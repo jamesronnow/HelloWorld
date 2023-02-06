30e0b84410ec0062d7d2fc84a20d0de2930cabed
James Ronnow <jronnow79@gmail.com>
2023-02-06 13:08:01 -0800

    code for printing date didn't push

--- src/edu/pcc/cis/helloworld/helloworld.java
+++ src/edu/pcc/cis/helloworld/helloworld.java
@@ -4,6 +4,6 @@ import java.util.Date;
 public class helloworld {
     public static void main(String[] args)  {
         System.out.println("Hello, World!");
-        System.out.println(new Date());
+        System.println(new Date());
     }
 }
